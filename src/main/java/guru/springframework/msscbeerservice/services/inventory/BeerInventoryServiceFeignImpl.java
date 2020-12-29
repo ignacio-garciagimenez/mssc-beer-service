bc01a8cdfca6635de99662ee73ce6e7090c09e44
Ignacio Garcia Gimenez <ignaciogarciagimenez@gmail.com>
2020-12-29 16:37:50 -0300

    Updated the inventory service rest call to use OpenFeign - closes #4

---
+++ src/main/java/guru/springframework/msscbeerservice/services/inventory/BeerInventoryServiceFeignImpl.java
@@ -0,0 +1,38 @@
+package guru.springframework.msscbeerservice.services.inventory;
+
+import guru.springframework.msscbeerservice.services.inventory.model.BeerInventoryDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Slf4j
+@RequiredArgsConstructor
+@Profile("local-discovery")
+@Service
+public class BeerInventoryServiceFeignImpl implements BeerInventoryService {
+
+    private final BeerInventoryFeignClient beerInventoryFeignClient;
+
+    @Override
+    public Integer getOnhandInventory(UUID beerId) {
+        log.debug("Calling Inventory Service");
+
+        ResponseEntity<List<BeerInventoryDto>> responseEntity = beerInventoryFeignClient.getOnHandInventory(beerId);
+
+        //sum from inventory list
+        Integer onHand = Objects.requireNonNull(responseEntity.getBody())
+                .stream()
+                .mapToInt(BeerInventoryDto::getQuantityOnHand)
+                .sum();
+
+        return onHand;
+    }
+}
